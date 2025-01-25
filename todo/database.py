37d457a7f60273cd54d291ada2a7065cb87735e9
zkkamir <zkkamir@gmail.com>
2025-01-25 07:58:24 +0330

    tried to fix db ignore

--- todo/database.py
+++ todo/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
