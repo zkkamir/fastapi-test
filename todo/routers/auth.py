f8cf70cf9c521f48804f1f1df6b767a78cd246ef
zkkamir <zkkamir@gmail.com>
2025-05-12 15:54:54 +0330

    Added change phone_number feature.

--- todo/routers/auth.py
+++ todo/routers/auth.py
@@ -28,6 +28,7 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+    phone_number: str
 
 
 class Token(BaseModel):
@@ -95,6 +96,7 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
         last_name=create_user_request.last_name,
         hashed_password=bcrypt_context.hash(create_user_request.password),
         role=create_user_request.role,
+        phone_number=create_user_request.phone_number,
     )
     db.add(create_user_model)
     db.commit()
--- todo/routers/users.py
+++ todo/routers/users.py
@@ -44,11 +44,23 @@ async def change_password(
 ):
     if user is None:
         raise HTTPException(401, "Authentication Failed")
-    user_model = db.query(Users).filter(Users.id == user.id).first()
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
     if not bcrypt_context.verify(
         user_verification.password, user_model.hashed_password
     ):
         raise HTTPException(401, "Error on password change")
     user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+
+
+@router.put("/phone_number/{phone_number}", status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(
+    user: user_dependency, db: db_dependency, phone_number: str
+):
+    if user is None:
+        raise HTTPException(401, "Authentication Failed")
+    user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+    user_model.phone_number = phone_number
     db.add(user_model)
     db.commit()
